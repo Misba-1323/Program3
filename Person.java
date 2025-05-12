7adc84c87fa2903510a34484a199f65cc1cca3ec
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-05-12 22:06:09 +0530

    save in main

---
+++ Person.java
@@ -0,0 +1,22 @@
+package Program3;
+
+import java.time.LocalDate; 
+import java.time.Period; 
+import java.time.format.DateTimeFormatter; 
+
+public class Person {
+
+String name; 
+LocalDate dob; 
+public Person(String name, String dobStr) { 
+this.name = name; 
+DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy"); 
+this.dob = LocalDate.parse(dobStr, formatter); 
+} 
+public void displayPerson() { 
+System.out.println("Person Name: " + name); 
+Period age = Period.between(dob, LocalDate.now()); 
+System.out.println("Person Age: " + age.getYears()); 
+} 
+}
+
